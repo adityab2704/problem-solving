ce7d414fe5164b25b7c7054341ea4684d82d9f0c
aditya Budh <adityabudh1234@gmail.com>
2023-08-08 12:52:49 +0530

    old rem commit

---
+++ 2-reverse-string.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+    let reversedStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      reversedStr += str[i];
+    }
+    return reversedStr;
+  }
+  
+  const originalString = "Hello, World!";
+  const reversedString = reverseString(originalString);
+  console.log(reversedString); // Output: "!dlroW ,olleH"
+  
